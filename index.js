bbd63975f7202bab60ba56653077bdd9aa0ff624
chwasiq0569 <samsunggrandprimeplus056@gmail.com>
2021-04-17 13:57:21 +0500

    Passed data from frontend to Backend

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require("express")
 
+const bodyParser = require("body-parser")
+
 const app = express()
 
 app.use(express.static("public"))
+app.use(bodyParser.json())
 
-app.get("/", (req, res) => {
-    res.send('HELLO')
+app.post("/admin/url", (req, res) => {
+    console.log(req.body)
+    res.send(req.body)
 })
 
 const port = process.env.PORT || 3000
